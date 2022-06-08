82e809a378de4822a925cceaa96bdf83c53abbbe
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-06-08 16:52:23 +0100

    -Code refactor

--- solver.py
+++ solver.py
@@ -1,7 +1,12 @@
 import argparse
 import os
 
+NO_SOLUTIONS = 'No solutions'
+
+MANY_SOLUTIONS = 'Infinitely many solutions'
+
 os.chdir("input")
+
 # Arg for command line
 parser = argparse.ArgumentParser()
 for arg in {'--infile', '--outfile'}:
@@ -9,8 +14,12 @@ for arg in {'--infile', '--outfile'}:
 args = parser.parse_args()
 
 
-# This function returns a correct display for the complex number
-def complex_printer(number):
+def complex_printer(number: complex) -> complex:
+    """
+    This function returns a correct display for the complex number
+    :param number:  Nombre complex  don't on veut effectuer un formatage
+    :return: Nombre complexe formaté
+    """
     comp = complex(number)
     if comp.real != 0:
         if comp.imag != 0:
@@ -19,32 +28,40 @@ def complex_printer(number):
     return complex(0, round(comp.imag, 4)) if comp.imag != 0 else 0
 
 
-# x+ay = 0 -> a = -(x / y)
-# Useful for determining the
-# coefficient to use to multiply a
-# row before adding to another
-def zero(x, y):
+def zero(x: complex, y: complex) -> complex:
+    """
+    x+ay = 0 -> a = -(x / y)
+    This function determine the a coefficient of this equation
+    Useful for determining the coefficient to use to multiply a row before adding to another
+    :param x: coefficient x of the equation
+    :param y: coefficient y of the equation
+    :return: number a of the equation
+    """
     return -(x / y)
 
 
-# ax = 1 -> a = 1/x
-# Useful for determining the
-# coefficient to use to multiply a
-# row to find 1 in pivot
-def one(x):
+def one(x: complex) -> complex:
+    """
+    ax = 1 -> a = 1/x
+    This function determine the a coefficient of this equation
+    Useful for determining the coefficient to use to multiply a row to find 1 in pivot
+    :param x: coefficient x of the equation
+    :return: contents a of the equation
+    """
     return 1 / x
 
 
 class Matrice:
-    # Constructor for matrix class
-    def __init__(self, rows, column, cont):
+
+    def __init__(self, rows: int, column: int, cont: list):
         self.rows, self.columns = rows, column
         self.contents = cont
 
 
 class SystemLinear:
-    # The system in matrix form: aX = b
-    def __init__(self, n, m, ab):
+    """The system in matrix form: aX = b"""
+
+    def __init__(self, n: int, m: int, ab):
         self.ab = ab
         self.n, self.m = n, m
         # self.permutation to contain column swap and
@@ -53,19 +70,28 @@ class SystemLinear:
         # if matrice is scaled
         self.echelon = False
 
-    # A matrix
     @property
     def a(self):
+        """
+        A matrix of systeme
+        :return: Return the A matrix of system
+        """
         return [x[:self.n] for x in self.ab]
 
-    # B vector
     @property
     def b(self):
+        """
+         B vector of systeme
+        :return: Return the B matrix of system
+        """
         return [x[self.n] for x in self.ab]
 
     @property
-    def rank(self):
-        # To check if matrix is scaled
+    def rank(self) -> str | int:
+        """
+        To check if matrix is scaled
+        :return: Rank of matrix
+        """
         if not self.echelon:
             return 'Non-scaled matrix'
         rank = self.m
@@ -74,14 +100,21 @@ class SystemLinear:
                 rank -= 1
         return rank
 
-    # multiply the line by the coefficient
     @staticmethod
-    def multiply_line(line, coefficient):
+    def multiply_line(line: list[complex], coefficient: complex) -> list[complex]:
+        """
+        multiply the line by the coefficient
+        :param line: line to multiply
+        :param coefficient: coefficient to multiply line by
+        :return: line with multiplied coefficient
+        """
         return [coefficient * x for x in line]
 
-    # Return L1 + (coefficient)L2
     @staticmethod
-    def add_line(line_1, line_2, coefficient):
+    def add_line(line_1: list[complex], line_2: list[complex], coefficient: complex) -> list[complex]:
+        """
+        :return:  L1 + (coefficient)L2
+        """
         return [x + (coefficient * y) for x, y in zip(line_1, line_2)]
 
     # Do L[x]<->L[y]
@@ -104,20 +137,19 @@ class SystemLinear:
     def null_pivot(self, x, base):
         if x == self.n - 1:
             return 0
-        else:
-            print(x)
-            for z in range(x + 1, self.m):
-                if self.ab[z][x] != 0:
-                    print(f'L{x} <-> L{z}')
-                    self.swap_line(base, z)
-                    return 2
-            for n in range(x + 1, self.n):
-                if self.ab[x][n] != 0:
-                    print(f'C{x}<-> C{n}')
-                    self.swap_column(base, n)
-                    self.permutation.append([x, n])
-                    return 2
-                self.null_pivot(x + 1, base)
+        print(x)
+        for z in range(x + 1, self.m):
+            if self.ab[z][x] != 0:
+                print(f'L{x} <-> L{z}')
+                self.swap_line(base, z)
+                return 2
+        for n in range(x + 1, self.n):
+            if self.ab[x][n] != 0:
+                print(f'C{x}<-> C{n}')
+                self.swap_column(base, n)
+                self.permutation.append([x, n])
+                return 2
+            self.null_pivot(x + 1, base)
         return 0
 
     def gauss_algorithm(self):
@@ -144,9 +176,7 @@ class SystemLinear:
         sol = [[0] for _ in range(self.n)]
         sol[self.n - 1] = [self.b[self.n - 1] / self.a[self.n - 1][self.n - 1]]
         for i in range(len(self.a) - 2, -1, -1):
-            somme = 0
-            for k in range(i + 1, self.n):
-                somme += self.a[i][k] * sol[k][0]
+            somme = sum(self.a[i][k] * sol[k][0] for k in range(i + 1, self.n))
             sol[i] = [(self.b[i] - somme) / self.a[i][i]]
         if self.counter != 0:
             self.sol_permutation(sol)
@@ -160,18 +190,12 @@ class SystemLinear:
         rank = self.rank
         print('Rang:', rank)
         if rank == self.m:
-            if rank == self.n:
-                return self.resolution()
-            else:
-                return 'Infinitely many solutions'
+            return self.resolution() if rank == self.n else MANY_SOLUTIONS
         if rank < self.m:
             for x in self.b[rank:]:
                 if x != 0:
-                    return 'No solutions'
-            if rank == self.n:
-                return self.resolution()
-            else:
-                return 'Infinitely many solutions'
+                    return NO_SOLUTIONS
+            return self.resolution() if rank == self.n else MANY_SOLUTIONS
 
 
 with open(args.infile, 'r') as infile, open(args.outfile, 'w') as outfile:
@@ -186,11 +210,11 @@ with open(args.infile, 'r') as infile, open(args.outfile, 'w') as outfile:
     sys_mat = [[complex(x) for x in y] for y in contents[1:]]
     system_lin = SystemLinear(int(contents[0][0]), int((contents[0][1])), sys_mat)
     soluce = system_lin.solution
-    if soluce == 'No solutions':
-        outfile.write('No solutions')
+    if soluce == NO_SOLUTIONS:
+        outfile.write(NO_SOLUTIONS)
         print(f'{soluce}')
-    elif soluce == 'Infinitely many solutions':
-        outfile.write('Infinitely many solutions')
+    elif soluce == MANY_SOLUTIONS:
+        outfile.write(MANY_SOLUTIONS)
         print(f'{soluce}')
     else:
 
